5f3680459aba76039324896198007dce99fdc5a2
Joseph Zilla <joseph.m.zilla@gmail.com>
2020-02-20 12:16:22 -0500

    Sites and Reservations obejcts and DAO

---
+++ src/main/java/com/techelevator/Reservations.java
@@ -0,0 +1,54 @@
+package com.techelevator;
+
+import java.util.Date;
+
+public class Reservations {
+	
+	private Long reservationId;
+	private Long siteId;
+	private String name;
+	private Date fromDate;
+	private Date toDate;
+	private Date createDate;
+	
+	
+	public Long getReservationId() {
+		return reservationId;
+	}
+	public void setReservationId(Long reservationId) {
+		this.reservationId = reservationId;
+	}
+	public Long getSiteId() {
+		return siteId;
+	}
+	public void setSiteId(Long siteId) {
+		this.siteId = siteId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+	public Date getToDate() {
+		return toDate;
+	}
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+
+}
