08d87f658765702016dfb2c3766fd837b7a97098
Jon McCollough <jonathonmccollough@gmail.com>
2020-02-21 11:46:35 -0500

    menu working up to reservations

--- src/main/java/com/techelevator/CampgroundCLI.java
+++ src/main/java/com/techelevator/CampgroundCLI.java
@@ -2,6 +2,7 @@ package com.techelevator;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import javax.sql.DataSource;
 
@@ -11,9 +12,9 @@ public class CampgroundCLI {
 
 	private ParkDAO parkDAO;
 	private CampgroundDAO campgroundDAO;
-	private SiteDAO siteDAO; 
+	private SiteDAO siteDAO;
 	private ReservationDAO reservationDAO;
-	
+
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/parks");
@@ -26,23 +27,72 @@ public class CampgroundCLI {
 
 	public CampgroundCLI(DataSource dataSource) {
 		parkDAO = new JDBCParkDAO(dataSource);
-		parkDAO.getAllParks();
-		parkDAO.getParkDetails(Long.valueOf(1));
 		campgroundDAO = new JDBCCampgroundDAO(dataSource);
-		campgroundDAO.getAllCampgroundsInfo(1);
 		siteDAO = new JDBCSiteDAO(dataSource);
-		siteDAO.viewAvailSitesByDate(1, LocalDate.of(2020,02,20), LocalDate.of(2020,3,23));
 		reservationDAO = new JDBCReservationDAO(dataSource);
-		reservationDAO.makeReservation(Long.valueOf(5), "TESTER", LocalDate.of(2020,02,20), LocalDate.of(2020,3,23));
-		
+
 	}
 
 	public void run() {
 
+		Scanner userInput = new Scanner(System.in);
+
+		boolean mainMenuControl = true;
+		while (mainMenuControl) {
+
+			System.out.println("National Parks Welcome Page");
+			System.out.println("Please selection your option");
+			System.out.println("1. Display National Parks");
+			System.out.println("2. Exit");
+
+			String welcomeScanner = userInput.nextLine();
+			int welcomeScannerInt = Integer.valueOf(welcomeScanner);
+
+			if (welcomeScannerInt != 1 && welcomeScannerInt != 2) {
+				System.out.println("Not a valid input");
+			}
+			if (welcomeScannerInt == 2) {
+				mainMenuControl = false;
+			}
+			if (welcomeScannerInt == 1) {
+				parkDAO.getAllParks();
+				System.out.println("Select park number to see more details: ");
+				System.out.println("Select 0 to go back.");
+
+				String secondScanner = userInput.nextLine();
+				int secondScannerInt = Integer.valueOf(secondScanner);
+
+				boolean secondControl = true;
+				if (secondScannerInt == 0) {
+					secondControl = false;
+				}
+				if (secondScannerInt != 1 && secondScannerInt != 2 && secondScannerInt != 3) {
+					System.out.println("Not a valid national park selection");
+				}
+				parkDAO.getParkDetails((long) secondScannerInt);
+				System.out.println("1. View Campgrounds");
+				System.out.println("0. Return to Previous Screen");
+
+				String thirdScanner = userInput.nextLine();
+				int thirdScannerInt = Integer.valueOf(thirdScanner);
+				if (thirdScannerInt == 0) {
+					secondControl = false;
+				}
+				
+				boolean thirdControl = true;
+				if (thirdScannerInt == 0) {
+					thirdControl = false;
+				}
+
+				if (thirdScannerInt == 1) {
+					campgroundDAO.getAllCampgroundsInfo(secondScannerInt);
+					System.out.println("1. Search for Available Reservation");
+					System.out.println("0. Return to Previous Screen");
+
+				}
+
+			}
+
+		}
 	}
-	
 }
-
-
-
-
---
+++ src/main/java/com/techelevator/ParksInput.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class ParksInput {
+	
+		Scanner userInput = new Scanner (System.in);
+
+
+		
+}
+
+	
+	
+	
+}
